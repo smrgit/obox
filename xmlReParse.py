b9f2c461afcdd6dc2b62bed486c5e32e31dddf50
smrgit <sheila_reynolds@smr-08sep19.c.psjh-mgl-reviewer-reporter.internal>
2019-10-12 13:25:55 +0000

    more changes -- added concept of a "container" node for which no unique identifier is needed and made some other tweaks to improve the information that goes into the table and should ease downstream analyses...

--- xmlReParse.py
+++ xmlReParse.py
@@ -66,7 +66,41 @@ def quickStrip(s):
 
 # ----------------------------------------------------------------------------------------------------
 
-def handleNodeData(node,gpID,pID):
+def isContainerNode(node):
+
+    ## print ( " CCCC : in isContainerNode ... " )
+
+    ## we'll start with an assumption that it IS just a container node ...
+    cFlag = True
+
+    if ( len(node.attrib) > 0 ):
+
+        for a in node.attrib:
+
+            textString = quickStrip(str(node.attrib[a]))
+            if ( len(textString)==0 or textString in naStrings ): 
+                skipFlag = True
+            else:
+                cFlag = False
+
+    textString = quickStrip(str(node.text))
+    if ( len(textString)==0 or textString in naStrings ):
+        skipFlag = True
+    else:
+        cFlag = False
+
+    ## print ( " CCCC : leaving isContainerNode ... ", cFlag )
+
+    return ( cFlag )
+
+# ----------------------------------------------------------------------------------------------------
+
+def handleNodeData(node,pID,eID):
+
+    ## print ( " DDDD : in handleNodeData ... ", pID, eID )
+
+    ## keep track of whether or not we have written something out for this node ...
+    outFlag = False
 
     if ( len(node.attrib) > 0 ):
 
@@ -75,62 +109,93 @@ def handleNodeData(node,gpID,pID):
 
         ## NB (!!!) attributes need to have their own 'bundle' identifier that is separate
         ## from the other 'contents' of this node...
-        aID = uuid.uuid4()
         for a in node.attrib:
 
-            qs = quickStrip(str(node.attrib[a]))
-            if ( len(qs)==0 or qs in naStrings ): 
+            textString = quickStrip(str(node.attrib[a]))
+            if ( len(textString)==0 or textString in naStrings ): 
                 skipFlag = True
+                ## print ( " DDDD :     skipping attribute <%s> " % a )
             else:
 
+                ## write out the 'attrib' contents of this particular node
                 longLabel = ''
                 if ( len(genList) > 0 ): longLabel = ' > '.join(genList) + ' > '
                 longLabel += node.tag + ' > ' + a 
+                longLabel = longLabel.strip()
+
+                ## print ( " DDDD :     writing attribute {%s = %s} " % ( longLabel, textString ) )
 
                 outString = inputFilename + '\t' + str(ithGen) + '\t' \
-                          + str(pID) + '\t' + str(aID) + '\t' \
+                          + str(pID) + '\t' + str(eID) + '\t' \
                           + longLabel + '\t' \
                           + node.tag + '\t' \
-                          + a + '\t' + qs 
+                          + a + '\t' + textString
 
                 outFh.write ( "%s\n" % outString )
+                outFlag = True
 
-    qs = quickStrip(str(node.text))
-    if ( len(qs)==0 or qs in naStrings ):
-        skipFlag = True
-    else:
+    textString = quickStrip(str(node.text))
+    if ( len(textString)==0 or textString in naStrings ):
+        ## if this node has no contents, we will still need to write 
+        ## out a line with a 'null' where the text would have been
+        ## so that will be able to track the bundle identifiers
+        textString = ''
 
-        longLabel = ''
-        if ( len(genList) > 0 ): longLabel = ' > '.join(genList) + ' > '
-        longLabel += node.tag 
+    ## print ( " DDDD :     textString = %s " % textString )
 
+    ## write out the text contents of this particular node
+    longLabel = ''
+    if ( len(genList) > 0 ): longLabel = ' > '.join(genList) + ' > '
+    longLabel += node.tag 
+    longLabel = longLabel.strip()
+
+    parentLabel = ''
+    if ( len(genList) > 0 ): parentLabel = genList[-1]
+
+    if ( (textString!='') or (not outFlag) ):
+        ## print ( " DDDD :     writing text {%s = %s} " % ( longLabel, textString ) )
         outString = inputFilename + '\t' + str(ithGen) + '\t' \
-                  + str(gpID) + '\t' + str(pID) + '\t' \
+                  + str(pID) + '\t' + str(eID) + '\t' \
                   + longLabel + '\t' \
-                  + genList[-1] + '\t' \
-                  + node.tag + '\t' + qs
-
+                  + parentLabel + '\t' \
+                  + node.tag + '\t' + textString
         outFh.write ( "%s\n" % outString )
+
+    ## print ( " DDDD : leaving handleNodeData ... " )
 
 # ----------------------------------------------------------------------------------------------------
 # inspired by:
 # https://stackoverflow.com/questions/28194703/recursive-xml-parsing-python-using-elementtree
 
-def handleNextLevel(root,gpID,pID):
+def handleNextLevel(root,pID):
 
     global ithGen, genList
 
     ithGen += 1
-    genList += [ root.tag ]
+    genList += [ root.tag.strip() ]
+
+    ## print ( " RRRR : in handleNextLevel ... ", ithGen, genList )
 
     ## loop over all children of the incoming node:
     for elem in root:
 
-        if ( ithGen <= maxDepth ):
-            handleNodeData(elem,gpID,pID)
+        ## is this node just a "container" node with no information???
+        if ( isContainerNode(elem) ):
+            if ( ithGen <= maxDepth ):
+                ## print ( " RRRR :    moving on to next level ... " )
+                handleNextLevel(elem,pID)
 
-        eID = uuid.uuid4()
-        handleNextLevel(elem,pID,eID)
+        else:
+
+            ## generate a new identifier for this node
+            eID = uuid.uuid4()
+            ## print ( " RRRR :    generating new identifier ... ", eID )
+
+            handleNodeData(elem,pID,eID)
+
+            ## if we haven't gone too deep yet...
+            if ( ithGen <= maxDepth ):
+                handleNextLevel(elem,eID)
 
     ## back up...
     ithGen -= 1
@@ -178,11 +243,11 @@ def main(args):
     root = tree.getroot()
     rootID = uuid.uuid4()
 
-    handleNodeData(root,rootID,rootID)
+    ## handle any information that exists at the root level ...
+    handleNodeData(root,None,rootID)
 
-    ## start by actually handling the root itself and its attributes, and then start 
-    ## digging down ...
-    handleNextLevel(root,rootID,rootID)
+    ## and then go to the next level (this is the recursive function)
+    handleNextLevel(root,rootID)
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
